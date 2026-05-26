7696cb161a70d48958ebefb815432f02ce009488
dineshchintapatla95-dotcom <dinesh.chintapatla95@gmail.com>
2026-05-26 09:49:17 +0530

    initial commit

---
+++ tests/test-2.spec.js
@@ -0,0 +1,26 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+  await page.getByRole('textbox', { name: 'Username' }).click();
+  await page.getByRole('textbox', { name: 'Username' }).fill('Admin');
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill('admin123');
+  await page.getByRole('button', { name: 'Login' }).click();
+  
+  await page.getByRole('link', { name: 'Admin' }).click();
+  await page.getByRole('button', { name: ' Add' }).click();
+  await page.getByText('-- Select --').first().click();
+  await page.getByRole('option', { name: 'Admin' }).click();
+  await page.getByRole('textbox', { name: 'Type for hints...' }).click();
+  await page.getByRole('textbox', { name: 'Type for hints...' }).fill('asish');
+  await page.getByText('-- Select --').click();
+  await page.getByRole('option', { name: 'Enabled' }).click();
+  await page.getByRole('textbox').nth(2).click();
+  await page.getByRole('textbox').nth(2).fill('Asish');
+  await page.getByRole('textbox').nth(3).click();
+  await page.getByRole('textbox').nth(3).fill('aish');
+  await page.getByRole('textbox').nth(4).click();
+  await page.getByRole('textbox').nth(4).fill('asish');
+  await page.getByRole('textbox').nth(4).fill('asish');
+});
