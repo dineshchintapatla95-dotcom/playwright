3080ddaad03969007c3d202dd8fa644f398fb226
dineshchintapatla95-dotcom <dinesh.chintapatla95@gmail.com>
2026-06-02 19:57:06 +0530

    master

--- tests/test-2.spec.js
+++ tests/test-2.spec.js
@@ -2,12 +2,13 @@ import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
   await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+  timeout: 5000;
   await page.getByRole('textbox', { name: 'Username' }).click();
   await page.getByRole('textbox', { name: 'Username' }).fill('Admin');
   await page.getByRole('textbox', { name: 'Password' }).click();
   await page.getByRole('textbox', { name: 'Password' }).fill('admin123');
-  await page.getByRole('button', { name: 'Login' }).click();
-  
+await page.getByRole('button', { name: 'Login' }).click();
+  timeout: 5000;
   await page.getByRole('link', { name: 'Admin' }).click();
   await page.getByRole('button', { name: ' Add' }).click();
   await page.getByText('-- Select --').first().click();
